23ecc22a439b6f763d7268272395901997010fb7
ks.krasssss <self.developmentkkras@gmail.com>
2026-04-16 02:32:36 +0300

    Update data, testing

--- UniversityProject/PostgraduateStudent.cpp
+++ UniversityProject/PostgraduateStudent.cpp
@@ -1,10 +1,10 @@
 #include "PostgraduateStudent.h"
 #include <iostream>
 
-PostgraduateStudent::PostgraduateStudent(std::string n, int a, std::string id, std::string topic)
+PostgraduateStudent::PostgraduateStudent(string n, int a, string id, string topic)
     : Student(n, a, id), thesisTopic(topic) {
 }
 
 void PostgraduateStudent::displayInfo() const {
-    std::cout << "Postgraduate Student: " << getName() << "\nTopic: " << thesisTopic << std::endl;
+    cout << "Postgrad: " << getName() << " | Thesis: " << thesisTopic << endl;
 }
--- UniversityProject/Student.h
+++ UniversityProject/Student.h
@@ -4,29 +4,22 @@
 #include "Person.h"
 #include "Grade.h"
 #include <vector>
-#include <string>
 
 class Student : public Person {
 private:
-    std::string studentID;
-    std::vector<Grade>* grades;
+    string studentID;
+    vector<Grade>* grades;
 
 public:
-    Student(std::string n, int a, std::string id);
-
-    // Копіювання та перенесення
+    Student(string n, int a, string id);
     Student(const Student& other);
     Student(Student&& other) noexcept;
     Student& operator=(const Student& rhs);
+    virtual ~Student();
 
-    void addGrade(std::string sub, int sc);
-    ~Student();
-
-	// Testing methods
+    void addGrade(string sub, int sc);
     int getGradesCount() const;
     bool isGradesEmpty() const;
-
-    std::string getName() const { return name; }
 };
 
 #endif
