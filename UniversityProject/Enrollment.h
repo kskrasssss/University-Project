58790874840227449a5e785c065ceb857d6359b3
ks.krasssss <self.developmentkkras@gmail.com>
2026-04-27 01:10:52 +0300

    Add IDisplayable.h interface and add some functions

--- UniversityProject/Enrollment.h
+++ UniversityProject/Enrollment.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "Student.h"
+#include "IDisplayable.h"
 
-class Enrollment {
+class Enrollment : public IDisplayable { // Теж наслідуємо інтерфейс
 public:
+    virtual ~Enrollment() {}
+    void displayInfo() const override { cout << "Admission Logic Object" << endl; }
+
     virtual bool checkEligibility(const Student& student) const = 0;
     virtual void printRequirements() const = 0;
-
-    virtual ~Enrollment() {}
 };
